01ae860a02a6f2369cddf8f8dcbdcbc553bfd182
mihir faujdar <faujdarmihir@gmail.com>
2022-01-27 20:23:12 +1000

    Update

---
+++ LightClientV2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace LightClientV2
+{
+    public class Program 
+    {    
+        private static ConsoleInterface UserInterface;
+
+        public static async Task Main(string[] args)
+        {
+            while (true)
+            {
+                UserInterface = new ConsoleInterface();
+                UserInterface.Display();
+                await UserInterface.MainMenu();
+            }
+            
+            
+        }
+
+        
+    }
+}
