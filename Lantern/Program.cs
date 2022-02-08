5061c927aee9a44dd40b8f93b8540b07e9b8b8eb
mihir faujdar <faujdarmihir@gmail.com>
2022-02-08 14:17:39 +1000

    Update

--- Lantern/Program.cs
+++ Lantern/Program.cs
@@ -1,6 +1,3 @@
-using Avalonia;
-using Avalonia.Controls;
-using Avalonia.Controls.ApplicationLifetimes;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
@@ -9,28 +6,15 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using LiteDB;
 
 namespace Lantern
 {
     public class Program
     {
-        // Initialization code. Don't use any Avalonia, third-party APIs or any
-        // SynchronizationContext-reliant code before AppMain is called: things aren't initialized
-        // yet and stuff might break.
-        [STAThread]
         public static void Main(string[] args)
         {
-            DisplayMessages();
-            CreateHostBuilder(args).Build().RunAsync();
-            BuildAvaloniaApp().StartWithClassicDesktopLifetime(args);
-        }
-
-        // Avalonia configuration, don't remove; also used by visual designer.
-        public static AppBuilder BuildAvaloniaApp()
-        {
-            return AppBuilder.Configure<App>()
-                .UsePlatformDetect()
-                .LogToTrace();
+            CreateHostBuilder(args).Build().Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
@@ -39,13 +23,5 @@ namespace Lantern
                 {
                     webBuilder.UseStartup<Startup>();
                 });
-
-        public static void DisplayMessages()
-        {
-            Console.ForegroundColor = ConsoleColor.Red;
-            Console.Write("\n\n** DO NOT CLOSE THIS WINDOW **");
-            Console.ForegroundColor = ConsoleColor.White;
-            Console.WriteLine("\n");
-        }
     }
 }
